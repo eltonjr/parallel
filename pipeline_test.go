1f51945c9d6db3b35fd599f54d934431c49f7e82
Elton Hoffmann <elton.hoffmann@mercadolivre.com>
2021-11-06 19:55:45 -0300

    Working with different types

--- pipeline_test.go
+++ pipeline_test.go
@@ -2,25 +2,31 @@ package parallel
 
 import (
 	"fmt"
+	"strings"
 	"testing"
 )
 
 func TestDo(t *testing.T) {
 	payload := []string{"abduct", "abducted", "abducting", "abduction", "abductions", "abductor", "abductores", "abductors", "abducts"}
-
-	p := NewPipeline(payload)
-	p = Do(reverse, p)
-	result := EndPipeline(p)
+	p1 := NewPipeline(payload)
+	p2 := Do(letterSum, p1)
+	result := EndPipeline(p2)
 
 	for _, r := range result {
 		fmt.Println(r)
 	}
 }
 
-func reverse(input string) string {
-	output := ""
-	for _, c := range input {
-		output = string(c) + output
+// sums every letter of the word
+// a=1, b=2, ...
+// so abc=(1+2+3)=6
+func letterSum(s string) int {
+	// in ascii table, letter a starts in position 97
+	// so we need to decrement 96
+	const asciiPad = 96
+	total := 0
+	for _, s := range strings.ToLower(s) {
+		total += int(s) - asciiPad
 	}
-	return output
+	return total
 }
